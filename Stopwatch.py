2c3c9230e37e7e3325d239d1d01f98b826687c40
neclor <81529592+neclor@users.noreply.github.com>
2022-01-24 17:10:47 +0500

    Update Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,6 +1,3 @@
-Time = 0
-Pause = 0
-
 def on_button_pressed_a():
     global Pause
     if Pause == 0:
@@ -14,6 +11,24 @@ def on_button_pressed_b():
     Time = 0
     Pause = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+Time = 0
+Pause = 0
+basic.show_leds("""
+    # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+""")
+basic.pause(200)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
 
 def on_forever():
     global Time
